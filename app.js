9d51dbdc874da31af2946480cb968920578439e3
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2024-08-06 21:15:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 // 
 //cors =>cross origin resource sharing
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['https://lms-frontend-roan.vercel.app'],
     credentials:true,
     methods:["POST","GET","DELETE","PUT"]
 }))
