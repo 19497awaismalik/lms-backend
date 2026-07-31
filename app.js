0225b6056cf124dfeed08272c9d6852153d57ce3
Awais Malik <165843085+19497awaismalik@users.noreply.github.com>
2026-07-31 16:54:22 +0500

    Update CORS configuration in app.js
    Commented out specific CORS configuration and enabled default CORS.

--- app.js
+++ app.js
@@ -33,11 +33,13 @@ app.use(cookieParser());
 
 // 
 //cors =>cross origin resource sharing
-app.use(cors({
-    origin:['https://lms-frontend-roan.vercel.app'],
-    credentials:true,
-    methods:["POST","GET","DELETE","PUT"]
-}))
+// app.use(cors({
+//     origin:['https://lms-frontend-roan.vercel.app'],
+//     credentials:true,
+//     methods:["POST","GET","DELETE","PUT"]
+// }))
+
+app.use(cors());
  
 //routes
 app.use('/api/v1',userRouter);
